6d49f642a8442f692d2c24c035973ab29c247b57
u4ik <you@example.com>
2022-06-09 18:34:50 -0400

    updated 6:34

--- tests/StreamingContentRepository.Tests/SC_REPO_TestingSite.cs
+++ tests/StreamingContentRepository.Tests/SC_REPO_TestingSite.cs
@@ -3,6 +3,22 @@ using System.Collections.Generic;
 
 public class SC_REPO_TestingSite
 {
+    private StreamingContent _content;
+    private StreamingContentRepository _repo;
+    public SC_REPO_TestingSite()
+    {
+        _content = new StreamingContent(
+            "Rubber",
+            "A tire that kills people",
+            5.8,
+            MaturityRating.R,
+            GenreType.Horror
+        );
+        _repo = new StreamingContentRepository();
+        _repo.AddContentToDirectory(_content);
+    }
+
+    //? CREATE
     [Fact]
     public void AddToDirectory_ShouldGetCorrectBoolean()
     {
@@ -20,8 +36,8 @@ public class SC_REPO_TestingSite
         Assert.Equal(expected, addResult);
     }
 
+    //? READ
     [Fact]
-
     public void GetDirectory_ShouldGetCorrectCollectionCount()
     {
         //? AAA Setup
@@ -39,4 +55,40 @@ public class SC_REPO_TestingSite
         Assert.Equal(expectedCount, contents.Count);
 
     }
+
+    //? READ
+    [Fact]
+    public void GetByTitle_ShouldReturnCorrectContent()
+    {
+        //? AAA Setup
+
+        //? Arrange is already taken care of in the constructor above^
+
+        //? Action
+        StreamingContent searchResult = _repo.GetContentByTitle("Rubber");
+
+        //? Assert
+        Assert.Equal(searchResult, _content);
+    }
+
+    //? UPDATE
+    [Fact]
+    public void UpdateExistingContent_ShouldReturnTrue()
+    {
+        //? Arrange
+        StreamingContent newContent = new StreamingContent(
+            "Modified Name",
+            "Modified Description",
+            1.0,
+            MaturityRating.PG_13,
+            GenreType.SciFi
+        );
+        //? Act
+        bool updatedResult = _repo.UpdateExistingContent("Rubber", newContent);
+
+        //? Assert  
+        Assert.True(updatedResult);
+    }
+
+
 }
